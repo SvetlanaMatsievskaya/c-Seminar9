950d9d2bf0b19fcfc5521605718a19d1552865e3
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-17 23:51:06 +0300

    Task64 completed

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int M = 0;
+int.TryParse(Console.ReadLine(), out M);
+int N = 0;
+int.TryParse(Console.ReadLine(), out N);
+
+string PrintNumbers(int M, int N)
+{
+if (M > N) 
+{
+    if (M==N)
+    {
+        return M.ToString();
+    }
+    return M + " " + PrintNumbers (M-1, N);    
+}
+else
+{
+    if (M == N)
+    {
+        return M.ToString();
+    }
+}
+    return M + " " + PrintNumbers (M+1, N);   
+}
+
+Console.WriteLine(PrintNumbers(M,N));
