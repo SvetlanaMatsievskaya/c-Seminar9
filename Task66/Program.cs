f3245627003b2e049d8ec1819e157491ae8499c0
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-18 00:02:30 +0300

    Task 64 competed

---
+++ Task66/Program.cs
@@ -0,0 +1,28 @@
+﻿int M = 0;
+int.TryParse(Console.ReadLine(), out M);
+int N = 0;
+int.TryParse(Console.ReadLine(), out N);
+
+int SumNumbers(int M, int N)
+{
+int sum = 0;
+if (M > N) 
+{
+    if (M==N)
+    {
+        return M;
+    }
+    return M + SumNumbers (M-1, N);    
+}
+else
+{
+    if (M == N)
+    {
+        return M;
+    }
+}
+    return M + SumNumbers (M+1, N);   
+}
+
+Console.WriteLine(SumNumbers(M,N));
+
