d4cb7a67c90ca460496773054b68565fccb14c92
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-18 00:10:55 +0300

    Task 68 updated

---
+++ Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿int M = 0;
+int.TryParse(Console.ReadLine(), out M);
+int N = 0;
+int.TryParse(Console.ReadLine(), out N);
+
+
+int rAckermann(int m, int n)
+{
+    
+    if (m == 0) 
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0) 
+    {
+        return Ackerman(m - 1, 1);
+    } 
+    else 
+    {
+        return Ackermann(m - 1, recursion(m, n - 1));
+    }
+}
+Console.WriteLine(Ackermann (M,N));
